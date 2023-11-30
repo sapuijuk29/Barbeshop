c3c76eeea0cbcb8777824fb931c2535d1a792216
sapuijuk29 <azaryaremon@gmail.com>
2023-11-30 23:58:37 +0700

    javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Add logic to handle form submission (e.g., send data to a server)
+    alert('Form submitted!');
+});
